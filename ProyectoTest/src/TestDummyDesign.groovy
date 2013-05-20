a98f8e0b8a555339d6825d3bc1370ddb19bbfcfe
JSorella <javiersorella@gmail.com>
2013-05-20 14:51:52 -0300

    - Volé comments

--- ProyectoTest/src/TestDummyDesign.groovy
+++ ProyectoTest/src/TestDummyDesign.groovy
@@ -4,9 +4,6 @@ import static junit.framework.Assert.*
 import org.junit.Before 
 import org.junit.Test
 
-//.... si les dice que no encuentra @Before y el @Test, es porque deben tener la librería del Junit 3.
-// El junit 4 está instalado.... hagan click derecho en Proyecto > Build Path > Library ... y ahí está para elegir
-
 class TestDummyDesign extends GroovyTestCase {
 
 	def integrante
